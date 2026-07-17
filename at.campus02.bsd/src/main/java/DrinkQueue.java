48bf8e91a92d30b9c7715685d2060aa635b2c854
Michaela Hofer <11745387@edu.campus02.at>
2026-07-17 16:22:21 +0200

    Implemented Methods for the drink queue

--- at.campus02.bsd/src/main/java/DrinkQueue.java
+++ at.campus02.bsd/src/main/java/DrinkQueue.java
@@ -1,4 +1,34 @@
 
-public class DrinkQueue implements Drink {
+public class DrinkQueue {
 
+    public boolean offer(Drink obj) {
+        if (elements.size() != maxSize) {
+            elements.add(obj);
+        } else {
+            return false;
+        }
+
+        return true;
+    }
+
+    public Drink poll() {
+        Drink element = peek();
+
+        if (elements.size() != 0) {
+            elements.remove(0);
+        }
+
+        return element;
+    }
+
+    public Drink remove() {
+        // Drink element = poll();
+        element = "";
+
+        if (element == null) {
+            throw new NoSuchElementException("There's no element anymore.");
+        }
+
+        return element;
+    }
 }
