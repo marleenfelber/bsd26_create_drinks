754c9b6addecf8a695f769c2f0953e5e20c671b2
Michaela Hofer <11745387@edu.campus02.at>
2026-07-17 18:04:37 +0200

    Test Coverage of SimpleDrink method

--- src/test/java/at/campus02/bsd/SimpleDrinkTest.java
+++ src/test/java/at/campus02/bsd/SimpleDrinkTest.java
@@ -1,5 +1,6 @@
 package at.campus02.bsd;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -10,15 +11,36 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
  * drinks
  */
 public class SimpleDrinkTest {
-    protected Liquid l;
+    protected Liquid liquidNoAlcohol;
+    protected Liquid liquidAlcohol;
+    protected SimpleDrink sodaZitron;
+    protected SimpleDrink aperolSpritz;
+
+
+    @BeforeEach
+    public void setUp(){
+        liquidNoAlcohol = new Liquid("Soda", 0.33, 0.0);
+        liquidAlcohol = new Liquid("Aperol", 1, 12.5);
+        sodaZitron = new SimpleDrink("Soda-Zitron", liquidNoAlcohol);
+        aperolSpritz = new SimpleDrink("Aperol Spritz", liquidAlcohol);
+    }
 
     @DisplayName ("Testing getVolume() method")
     @Test
     void testGetVolume() {
-        Liquid liquid = new Liquid("Water", 0.5, 0);
-        SimpleDrink drink = new SimpleDrink("Mineral Water", liquid);
+        assertEquals(0.33, sodaZitron.getVolume());
+    }
 
-        assertEquals(0.5, drink.getVolume());
+    @DisplayName("Testing getAlcoholPercent() method")
+    @Test
+    void testGetAlcoholPercent() {
+        assertEquals(12.5, aperolSpritz.getAlcoholPercent());
+    }
+
+    @DisplayName("Testing isAlcoholic()")
+    @Test
+    void testIsAlcoholic() {
+        assertEquals(true, aperolSpritz.isAlcoholic());
     }
 
 }
